a13bc1f7d0273c445c200bbec9b7c96c77d05d6c
Subhranshu Sekhar Choudhury <subhransuchoudhury00@gmail.com>
2022-06-23 20:18:03 +0530

    Ajax for POST req. No page loading
    - user can now see names of the user joined the room.
    - no page reload while sending messages.

--- app.js
+++ app.js
@@ -12,26 +12,26 @@ const app = express();
 
 function timestamp() {
     let d = new Date();
-    let ISTTime = new Date(d.getTime() + (330 + d.getTimezoneOffset())*60000);
-    let timeStamp = `[${['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'][ISTTime.getDay()]} ${ISTTime.getHours() % 12 || 12}:${ISTTime.getMinutes()} ${ISTTime.getHours()>=12?"PM":"AM"} ${d.getDate()}/${d.getMonth()+1}/${d.getFullYear()} IST]`;
+    let ISTTime = new Date(d.getTime() + (330 + d.getTimezoneOffset()) * 60000);
+    let timeStamp = `[${['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'][ISTTime.getDay()]} ${ISTTime.getHours() % 12 || 12}:${ISTTime.getMinutes()} ${ISTTime.getHours() >= 12 ? "PM" : "AM"} ${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()} IST]`;
     return timeStamp;
 }
 
 
 
 app.use(express.static("public"));
-app.set("view engine","ejs");
-app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(session({
     secret: process.env.SECRET_KEY,
     resave: false,
     saveUninitialized: false
     // cookie: { secure: true }
-  }));
+}));
 
-  app.use(passport.initialize());
-  app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect(`mongodb+srv://admin_subhranshu:${process.env.DB_KEY}@cluster0.one0j.mongodb.net/anonymousChat`);
 
@@ -44,110 +44,110 @@ const roomSchema = new mongoose.Schema({
     roomname: String,
     password: String,
     messages: [String],
-    
+
 });
 
 
 userSkeliton.plugin(passportLocalMongoose);
 
 
-const Room = new mongoose.model("room",roomSchema);
-const User = new mongoose.model("user",userSkeliton);
+const Room = new mongoose.model("room", roomSchema);
+const User = new mongoose.model("user", userSkeliton);
 
 passport.use(User.createStrategy());
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.render("home");
 });
 
-app.use((req, res, next) => { res.header({"Access-Control-Allow-Origin": "*"}); next(); })
+app.use((req, res, next) => { res.header({ "Access-Control-Allow-Origin": "*" }); next(); })
 
-app.get("/login",(req,res)=>{
+app.get("/login", (req, res) => {
     res.render("login");
 });
 
-app.get("/register",(req,res)=>{
+app.get("/register", (req, res) => {
     res.render("register");
 });
 
-app.get("/room",(req,res)=>{
-    
-    if(req.isAuthenticated()){
-        res.render("room",{username:req.user.username});
-    }else{
+app.get("/room", (req, res) => {
+
+    if (req.isAuthenticated()) {
+        res.render("room", { username: req.user.username });
+    } else {
         res.redirect("/login");
     }
 });
 
-app.get("/logout",(req,res)=>{
-    req.logout((err)=>{
-        if(err){
+app.get("/logout", (req, res) => {
+    req.logout((err) => {
+        if (err) {
             console.log(err);
             res.send("Something went wrong! Kindy retry or conatct subhranshu choudhury.")
-        }else{
-    res.redirect("/");
+        } else {
+            res.redirect("/");
 
         }
     });
 });
 
-app.post("/register",(req,res)=>{
-  
-    User.register({username: req.body.username},req.body.password,(err,u)=>{
-        if(err){
+app.post("/register", (req, res) => {
+
+    User.register({ username: req.body.username }, req.body.password, (err, u) => {
+        if (err) {
             console.log(err);
             res.redirect("/register");
-        }else{
-            passport.authenticate("local")(req,res,()=>{
+        } else {
+            passport.authenticate("local")(req, res, () => {
                 res.redirect("/room");
             });
         }
     });
 
-    
+
 });
 
-app.post("/login",(req,res)=>{
-   const user = new User({
-    username:req.body.username,
-    password:req.body.password
-   });
+app.post("/login", (req, res) => {
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
 
-   req.login(user,(err)=>{
-    if(err){
-        console.log(err);
-    }else{
-        passport.authenticate("local")(req,res,()=>{
-            res.redirect("/room");
-        });
-    }
-   });
-    
+    req.login(user, (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            passport.authenticate("local")(req, res, () => {
+                res.redirect("/room");
+            });
+        }
+    });
+
 });
 
 // ROOM 
 
-app.get("/create-room",(req,res)=>{
-    if(req.isAuthenticated()){
+app.get("/create-room", (req, res) => {
+    if (req.isAuthenticated()) {
         res.render("create-room");
-    }else{
+    } else {
         res.redirect("/login");
     }
 })
 
-app.post("/create-room",(req,res)=>{
+app.post("/create-room", (req, res) => {
     const newRoom = new Room({
         roomname: req.body.roomname,
         password: req.body.password
     });
 
-    newRoom.save((err)=>{
-        if(err){
+    newRoom.save((err) => {
+        if (err) {
             res.send(err);
-        }else{
+        } else {
             res.redirect("/join-room");
         }
 
@@ -156,114 +156,115 @@ app.post("/create-room",(req,res)=>{
 
 
 
-app.get("/chat/:roomName/:roomPassword",(req,res)=>{
-    if(req.isAuthenticated()){
-        Room.findOne({roomname:req.params.roomName,password:req.params.roomPassword},(err,message)=>{
-            if(message){
+app.get("/chat/:roomName/:roomPassword", (req, res) => {
+    if (req.isAuthenticated()) {
+        Room.findOne({ roomname: req.params.roomName, password: req.params.roomPassword }, (err, message) => {
+            if (message) {
                 const userMessage = message.messages;
-                res.render("chat",{roomName:req.params.roomName,roomPassword:req.params.roomPassword,userMessage:userMessage});
-    
-            }else{
-                if(err){
+                res.render("chat", { roomName: req.params.roomName, roomPassword: req.params.roomPassword, userMessage: userMessage });
+
+            } else {
+                if (err) {
                     res.send(err);
-                }else{
-                    res.render("result",{message:"Room has been deleted or not found."});
+                } else {
+                    res.render("result", { message: "Room has been deleted or not found." });
                 }
             }
         });
-        
-    }else{
+
+    } else {
         res.redirect("/login");
     }
 });
 
-app.post("/chat/:roomName/:roomPassword",(req,res)=>{
+app.post("/chat/:roomName/:roomPassword", (req, res) => {
 
-    if(req.isAuthenticated()){
+    if (req.isAuthenticated()) {
         const message = req.body.message;
         const username = req.user.username;
-        Room.updateOne({roomname:req.params.roomName,password:req.params.roomPassword},{$push: { messages: `${timestamp()}<br><i>[${username}]</i> :- <b>${message}</b>` }},(err)=>{
-           if(err){
-               res.send(err);
-           }else{
-               res.redirect(`/chat/${req.params.roomName}/${req.params.roomPassword}`);
-            
-           }
-       });
-    }else{
+        Room.updateOne({ roomname: req.params.roomName, password: req.params.roomPassword }, { $push: { messages: `${timestamp()}<br><i>[${username}]</i> :- <b>${message}</b>` } }, (err) => {
+            if (err) {
+                res.send(err);
+            } else {
+                res.redirect(`/chat/${req.params.roomName}/${req.params.roomPassword}`);
+
+            }
+        });
+    } else {
         res.redirect("/login");
     }
 
-    
+
 });
 
-app.get("/join-room",(req,res)=>{
-    if(req.isAuthenticated()){
+app.get("/join-room", (req, res) => {
+    if (req.isAuthenticated()) {
         res.render("join-room");
-    }else{
+    } else {
         res.redirect("login");
     }
 });
 
-app.post("/join-room",(req,res)=>{
-    if(req.isAuthenticated()){
+app.post("/join-room", (req, res) => {
+    if (req.isAuthenticated()) {
         const roomname = req.body.roomname;
         const password = req.body.password;
-        Room.findOne({roomname: roomname,password:password},(err,foundRoom)=>{
-            if(foundRoom){
+        Room.findOne({ roomname: roomname, password: password }, (err, foundRoom) => {
+            if (foundRoom) {
                 res.redirect(`/chat/${roomname}/${password}`);
 
-            }else{
-                if(err){
+            } else {
+                if (err) {
                     res.send(err);
-                }else{
-                    res.render("result",{message:"Room Name or Room Password mismatched. Kindly Recheck."});
+                } else {
+                    res.render("result", { message: "Room Name or Room Password mismatched. Kindly Recheck." });
                 }
             }
         });
 
         ;
-    }else{
+    } else {
         res.redirect("login");
     }
 });
 
-app.get("/delete-room",(req,res)=>{
+app.get("/delete-room", (req, res) => {
     res.render("delete-room")
 });
 
-app.post("/delete-room",(req,res)=>{
+app.post("/delete-room", (req, res) => {
     const roomName = req.body.roomName;
     const roomPassword = req.body.roomPassword;
-    
-    Room.deleteOne({roomname:roomName,password:roomPassword},(err)=>{
+
+    Room.deleteOne({ roomname: roomName, password: roomPassword }, (err) => {
         if (err) {
             res.send(err);
         } else {
-            res.render("result",{message:"Room has been deleted successfully."})
+            res.render("result", { message: "Room has been deleted successfully." })
         }
     })
 })
 
-app.get("/messages/:roomName/:roomPassword",(req,res)=>{
+app.get("/messages/:roomName/:roomPassword", (req, res) => {
     const roomName = req.params.roomName;
     const roomPassword = req.params.roomPassword;
-    
-    Room.findOne({roomname:roomName,password:roomPassword},(err,message)=>{
-        if(message){
+
+    Room.findOne({ roomname: roomName, password: roomPassword }, (err, message) => {
+        if (message) {
             res.send(message.messages);
 
-        }else{
-            if(err){
+        } else {
+            if (err) {
                 res.send(err);
-            }else{
-                res.render("result",{message:"No room matched. Recheck Room Name & Room Password."});
+            } else {
+                res.render("result", { message: "No room matched. Recheck Room Name & Room Password." });
             }
         }
     });
 });
 
 
-app.listen(process.env.PORT || 3000,()=>{
+
+app.listen(process.env.PORT || 3000, () => {
     console.log("===> Live at port 3000")
 });
